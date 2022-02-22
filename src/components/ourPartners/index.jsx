1d1610cd3d2076e77c9e07231fdfcf25f067ae06
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-02-22 21:34:27 +0500

    Our partners icons size decreased

--- src/components/ourPartners/index.jsx
+++ src/components/ourPartners/index.jsx
@@ -3,7 +3,7 @@ import { Zoom } from "react-reveal";
 
 const SinglePartner = ({ className, alt, Src }) => (
   <div
-    className={`${className} single_partner_div flex-[1_0_21%] flex items-center justify-center min-w-[200px] min-h-[200px]  mx-4 p-2`}
+    className={`${className} single_partner_div flex-[1_0_16%] flex items-center justify-center min-w-[120px] min-h-[120px]  mx-4 p-2`}
   >
     <Src />
   </div>
@@ -11,22 +11,24 @@ const SinglePartner = ({ className, alt, Src }) => (
 
 const OurPartners = () => {
   return (
-    <Zoom  left>
+    <Zoom left>
       <section className="p-20 px-12 min-h-screen md:h-screen">
-        <div className=" flex justify-center mb-24 items-center">
-          <div  >
-          <h1 className="tracking-[.7rem] uppercase ml-18  mb-24font-light text-2xl md:text-2xl text-center">
-         OUR PARTNERS          
-         </h1>
+        <div className=" flex justify-center mb-12 items-center">
+          <div>
+            <h1 className="tracking-[.7rem] uppercase ml-18 mb-24 font-light text-2xl md:text-2xl text-center">
+              Stratos partners with companies committed to diversifying their
+              workforce.
+            </h1>
           </div>
         </div>
-        <div  className="  mb-24 flex justify-center items-center h-6/6">
+        <div className="  mb-24 flex justify-center items-center h-6/6">
           <div className="flex flex-wrap items-center justify-center mb-20 lg:mt-0">
             <SinglePartner
               Src={require("../../assets-partners/acadia-hover.svg").default}
               alt="Partner 1"
             />
             <SinglePartner
+              // className="h-8 w-8"
               Src={require("../../assets-partners/bevy-hover.svg").default}
               alt="Partner 2"
             />
@@ -51,63 +53,51 @@ const OurPartners = () => {
               Src={require("../../assets-partners/verkada-hover.svg").default}
               alt="Partner 7"
             />
-             <SinglePartner
+            <SinglePartner
               Src={require("../../assets-partners/no-name1.svg").default}
               alt="Partner 8"
-            /> 
-            
-             <SinglePartner
+            />
+
+            <SinglePartner
               Src={require("../../assets-partners/no-name2.svg").default}
               alt="Partner 9"
-
             />
             <SinglePartner
               Src={require("../../assets-partners/no-name3.svg").default}
               alt="Partner 10"
             />
-
-    
-
           </div>
         </div>
         <div className="flex justify-center items-center mr-7">
           <a href="https://www.instagram.com/trystratos/">
-              <img 
-              className="w-[45px]"
-              src="assets/insta.png"
-              />
+            <img className="w-[45px]" src="assets/insta.png" />
           </a>
           <a href="https://www.linkedin.com/company/trystratos/">
-          <img
-            className="w-[45px]"
-            src="assets/linkedin.png"                 
-            />
+            <img className="w-[45px]" src="assets/linkedin.png" />
           </a>
           <a href="mailto:info@trystratos.com">
-          <img
-            className="w-[40px]"
-            src="assets/mail.png"                 
-            />
+            <img className="w-[40px]" src="assets/mail.png" />
           </a>
-          </div>
-          <div className="flex justify-center items-center">
-          <h1 className="tracking-[.7rem] uppercase ml-55 font-light text-1xl md:text-1xl text-center">
-          Stratos Solutions </h1>
-          <br>
-          </br>
-          </div>
-          <div className="flex items-center justify-center">
-          <h3  className="tracking-[.7rem]  ml-30 uppercase font-light text-1xl md:text-1xl text-center">
-          120 W Trinity Pl, Decatur, GA 30030 </h3>
-          </div>
-          <div>
-          <div className="flex items-center justify-center">
-           <h4  className="tracking-[.7rem] uppercase font-light text-1xl md:text-1xl text-center">
-          404-530-9642 </h4>
-          </div>
-          </div>
-         <div>
         </div>
+        <div className="flex justify-center items-center">
+          <h1 className="tracking-[.7rem] uppercase ml-55 font-light text-1xl md:text-1xl text-center">
+            Stratos Solutions{" "}
+          </h1>
+          <br></br>
+        </div>
+        <div className="flex items-center justify-center">
+          <h3 className="tracking-[.7rem]  ml-30 uppercase font-light text-1xl md:text-1xl text-center">
+            120 W Trinity Pl, Decatur, GA 30030{" "}
+          </h3>
+        </div>
+        <div>
+          <div className="flex items-center justify-center">
+            <h4 className="tracking-[.7rem] uppercase font-light text-1xl md:text-1xl text-center">
+              404-530-9642{" "}
+            </h4>
+          </div>
+        </div>
+        <div></div>
       </section>
     </Zoom>
   );
